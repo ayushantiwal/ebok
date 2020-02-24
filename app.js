94ae64a5c9eb3be18f90c157692c15d01dabf609
Antiwal <ayushantiwal521@gmail.com>
2020-02-24 14:27:15 +0530

    fixed error

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ var userRoutes = require("./routes/user");
 
 
 var app = express();
-mongoose.connection(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 require("./config/passport");
 
 // view engine setup
